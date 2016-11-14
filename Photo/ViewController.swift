9099ad8142c34bc71ce0ac74635b927289835324
sqluo <sqluo@sqluodeMac-mini.local>
2016-11-14 11:06:27 +0800

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by sqluo on 2016/11/14.
+//  Copyright © 2016年 sqluo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
